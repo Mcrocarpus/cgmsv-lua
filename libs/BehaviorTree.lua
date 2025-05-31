3ff372d3d804b662e45459991a62b96db2ce73e9
念小白 <174920330@qq.com>
2025-05-31 22:58:01 +0800

    [#] 更新行为树模块

--- libs/BehaviorTree.lua
+++ libs/BehaviorTree.lua
@@ -1,3 +1,14 @@
-_G.behavior_tree = dofile('lua/libs/behavior3lua/behavior3/behavior_tree.lua')
-_G.behavior_node = dofile('lua/libs/behavior3lua/behavior3/behavior_node.lua')
-_G.behavior_process = dofile('lua/libs/behavior3lua/behavior3/sample_process.lua')
+BT = {}
+_G.BT = BT
+
+BT.Tree = dofile('lua/libs/behavior3lua/behavior3/behavior_tree.lua')
+BT.Node = dofile('lua/libs/behavior3lua/behavior3/behavior_node.lua')
+BT.Process = dofile('lua/libs/behavior3lua/behavior3/sample_process.lua')
+
+function BT.loadTree(path)
+    local file, err = io.open(path, 'r')
+    assert(file, err)
+    local str = file:read('*a')
+    file:close()
+    return JSON.decode(str)
+end
