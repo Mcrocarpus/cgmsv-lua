7bd73190581b562392ce8fe0dda73f80a214c780
念小白 <174920330@qq.com>
2025-05-31 21:53:55 +0800

    [+] BehaviorTree.lua

---
+++ libs/BehaviorTree.lua
@@ -0,0 +1,3 @@
+_G.behavior_tree = dofile('lua/libs/behavior3lua/behavior3/behavior_tree.lua')
+_G.behavior_node = dofile('lua/libs/behavior3lua/behavior3/behavior_node.lua')
+_G.behavior_process = dofile('lua/libs/behavior3lua/behavior3/sample_process.lua')
